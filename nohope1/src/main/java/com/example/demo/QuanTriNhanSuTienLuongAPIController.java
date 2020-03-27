e034403b6418653ea228396cca4f8865b94b97ce
hieu2211 <h.vanhieu1999@gmail.com>
2020-03-27 12:43:46 +0700

    hieu

--- nohope1/src/main/java/com/example/demo/QuanTriNhanSuTienLuongAPIController.java
+++ nohope1/src/main/java/com/example/demo/QuanTriNhanSuTienLuongAPIController.java
@@ -1,30 +1,32 @@
 package com.example.demo;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/quantri")
 public class QuanTriNhanSuTienLuongAPIController {
 	@Autowired
-	daoBangLuong bangluong;
-	@Autowired
-	daoBoPhan bophan;
-	@Autowired
-	daoChamCong chamcong;
-	@Autowired
-	daoChucVu chucvu;
-	@Autowired
-	daoHopDong hopdong;	
-	@Autowired
-	daoNguoiDung nguoidung;
-	@Autowired
-	daoNhanVien nhanvien;
-	@Autowired
-	daoPhongBan phongban;
-	@Autowired
-	daoQuyenHang quyenhang;
-	@Autowired
-	daoTinhLuong tinhluong;
-	@Autowired
-	daoTrinhDo trinhdo;
+	QuanTriNhanSuTienLuongAPService service;
+
+	// 1 Get all data nhan vien
+	// link: QuanTriNhanSuTienLuong/html/listNV.html
+	@CrossOrigin(value = "*")
+	@RequestMapping(value = "/getAllDataNhanVien")
+	public List<NhanVien> getAllDataNhanVien() {
+		return service.getAllDataNhanVien();
+	}
+
+	// 1 Get all data chucvu
+	// link: QuanTriNhanSuTienLuong/html/listCV.html
+	@CrossOrigin(value = "*")
+	@RequestMapping(value = "/getAllDataChucVu")
+	public List<ChucVu> getAllDataChucVu() {
+		return service.getAllDataChucVu();
+	}
+
 }
--- nohope1/src/main/java/com/example/demo/QuanTriNhanSuTienLuongAPService.java
+++ nohope1/src/main/java/com/example/demo/QuanTriNhanSuTienLuongAPService.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,5 +10,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value = "/quantrinhansutienluong")
 public class QuanTriNhanSuTienLuongAPService {
 	@Autowired
-	QuanTriNhanSuTienLuongAPService service;
+	daoBangLuong bangluong;
+	@Autowired
+	daoBoPhan bophan;
+	@Autowired
+	daoChamCong chamcong;
+	@Autowired
+	daoChucVu chucvu;
+	@Autowired
+	daoHopDong hopdong;
+	@Autowired
+	daoNguoiDung nguoidung;
+	@Autowired
+	daoNhanVien nhanvien;
+	@Autowired
+	daoPhongBan phongban;
+	@Autowired
+	daoQuyenHang quyenhang;
+	@Autowired
+	daoTinhLuong tinhluong;
+	@Autowired
+	daoTrinhDo trinhdo;
+
+	// 1 Get all data nhanvien
+	public List<NhanVien> getAllDataNhanVien() {
+		return nhanvien.findAll();
+	}
+
+	// 1 Get all data chucvu
+	public List<ChucVu> getAllDataChucVu() {
+		return chucvu.findAll();
+	}
 }
